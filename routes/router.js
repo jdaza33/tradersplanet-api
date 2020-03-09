daa89f3d278484496c4a458d984034c7b707af1a
J. Daza <blackencio33@gmail.com>
2020-03-09 17:06:02 -0400

    cambios y correcciones

--- routes/router.js
+++ routes/router.js
@@ -50,7 +50,7 @@ router.post(
   _upload.single('file'),
   userCtrl.setPhoto
 )
-router.get('/users/pay/course/:id/:courseId', userCtrl.payCourse)
+router.put('/users/pay/course/:id/:courseId', userCtrl.payCourse)
 
 /** Lessons */
 router.post('/lessons/create', lessonCtrl.create)
