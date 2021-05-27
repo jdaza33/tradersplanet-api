8a6936960d1f129f2d5e617de2435590f5a6e330
J. Daza <blackencio33@gmail.com>
2021-05-27 03:53:26 -0400

    avances 27052021

--- discord.js
+++ discord.js
@@ -4,7 +4,7 @@ const client = new Discord.Client()
 
 const guildTplanet = new Discord.Guild(client)
   .addMember('831773022800576533', {
-    accessToken: 'gMcyhEoGTe1ovY5FVb1ZWtkkpk4tz8',
+    accessToken: '',
   })
   .then((res) => console.log(res))
   .catch((err) => console.log(err))
@@ -13,4 +13,4 @@ const guildTplanet = new Discord.Guild(client)
 
 // console.log(users.id)
 
-client.login('ODMxNzQyNjYxMDQ3MDkxMjIw.YHZqtw.omigYoC6zW8S60L5JTXeYkhkyNY')
+client.login('')
